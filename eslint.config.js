461981bca24e7c53599fd3c9b33c9fff299f696e
Deonyss17 <denys.lachyn.pp.2023@lpnu.ua>
2026-04-07 21:56:17 +0300

    Initial commit: setup UniDone project structure, add README and .gitignore

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+  {
+    ignores: ['dist/', 'node_modules/']
+  }
+]
